0ab9e7e221076fe9e260c59cfbed0225a8e846b7
Denys Parchenko <denis.parchenko@gmail.com>
2018-05-23 14:54:16 +0200

    Revert "Add ordinal value to Rank enum"
    This reverts commit 2f69df47a35d3986fb715d4c7a6eff38a3cc93d8.

--- holdem/src/main/scala/com/weeklyhacks/holdem/Rank.scala
+++ holdem/src/main/scala/com/weeklyhacks/holdem/Rank.scala
@@ -1,21 +1,14 @@
 package com.weeklyhacks.holdem
 
 object Rank extends Enumeration {
-  protected case class Val(name: String, ordinalValue: Int) extends super.Val {
-    override def toString(): String = name
-  }
-
-  val _2 = Val("2", 1)
-  val _3 = Val("3", 2)
-  val _4 = Val("4", 3)
-  val _5 = Val("5", 4)
-  val _6 = Val("6", 5)
-  val _7 = Val("7", 6)
-  val _8 = Val("8", 7)
-  val _9 = Val("9", 8)
-  val _10 = Val("10", 9)
-  val J = Val("J", 10)
-  val Q = Val("Q", 11)
-  val K = Val("K", 12)
-  val A = Val("A", 13)
+  val _2 = Value("2")
+  val _3 = Value("3")
+  val _4 = Value("4")
+  val _5 = Value("5")
+  val _6 = Value("6")
+  val _7 = Value("7")
+  val _8 = Value("8")
+  val _9 = Value("9")
+  val _10 = Value("10")
+  val J, Q, K, A = Value
 }
